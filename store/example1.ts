15bcbf49fc702f679134be3f975f104e0bf2ca32
atibway <ainebyoonaatiidu@gmail.com>
2024-10-12 09:37:21 +0300

    hi

--- store/example1.ts
+++ store/example1.ts
@@ -15,3 +15,11 @@ export const useStore = create<storeProps>((set)=> ({
     removeAllCount: () => set({count: 0}),
     updateCount: (newCount: number) => set({count: newCount})
 }))
+
+export const useStores = create<storeProps>((set)=> ({
+    count: 0,
+    increase: ()=> set((state)=> ({count: state.count + 1})),
+    decrease: ()=> set((state)=> ({count: state.count - 1})),
+    removeAllCount: () => set({count: 0}),
+    updateCount: (newCount: number) => set({count: newCount})
+}))
